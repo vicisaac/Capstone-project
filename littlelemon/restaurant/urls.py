fe023932b1539ece49f5536e74d15ecb812780be
vicisaac <61437599+vicisaac@users.noreply.github.com>
2026-01-02 10:33:59 +0100

    Added user serilizer and views

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,16 @@
 #define URL route for index() view
 from django.urls import path
 from . import views
+#from .views import UserListView, UserDetailView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # URL for listing all users
+    #path('users/', UserListView.as_view(), name='user-list'),
+    
+    # URL for retrieving a specific user by username
+    #path('users/<str:username>/', UserDetailView.as_view(), name='user-detail'),
 ]
 
 # The index() view renders the 'index.html' template when the root URL is accessed.
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# UserViewSet handles CRUD operations for the User model
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()  # Queryset to retrieve all users
+    serializer_class = UserSerializer  # Serializer to use for User model
